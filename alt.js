0033a49d4e21bdd2ff5ba9c69ea679616d4c6841
arestenkos <arestenkos@gmail.com>
2024-05-27 10:36:46 +0300

    change alts

--- alt.js
+++ alt.js
@@ -1,1 +1,25 @@
-console.log('alt');
+(function () {
+  window.onload = async function () {
+    function addAlt(el) {
+        console.log(el, alt);
+    }
+
+     function getAlts(number = 1) {
+        const url = 'https://random-word-api.herokuapp.com/word?number='
+        return  fetch(url + number).then(response => response.json());
+
+    }
+
+    const imgs = document.querySelectorAll('img');
+    const imgsLength = imgs.length;
+    const alts = await getAlts(imgsLength);
+
+    for(let i = 0; i < imgsLength; i +=1) {
+        const img = imgs[i];
+        img.alt = alts[i]
+
+    }
+
+
+};
+})();
