62fc1071cdad0f2e62e4626012d97a025b359d94
arestenkos <arestenkos@gmail.com>
2024-05-27 11:46:08 +0300

    altIsChanging

--- alt.js
+++ alt.js
@@ -1,25 +1,100 @@
 (function () {
   window.onload = async function () {
-    function addAlt(el) {
-        console.log(el, alt);
+    // temporary disable scroll
+
+    function disableBodyScrolling() {
+      const clientWidthBefore = document.body.clientWidth;
+      document.body.style.overflow = "hidden";
+
+      const clientWidthAfter = document.body.clientWidth;
+
+      if (clientWidthBefore !== clientWidthAfter) {
+        document.body.style.paddingRight = `${
+          clientWidthAfter - clientWidthBefore
+        }px`;
+      }
     }
 
-     function getAlts(number = 1) {
-        const url = 'https://random-word-api.herokuapp.com/word?number='
-        return  fetch(url + number).then(response => response.json());
-
+    function enableBodyScrolling() {
+      document.body.style.overflow = "";
+      document.body.style.paddingRight = "";
     }
 
-    const imgs = document.querySelectorAll('img');
-    const imgsLength = imgs.length;
-    const alts = await getAlts(imgsLength);
-
-    for(let i = 0; i < imgsLength; i +=1) {
-        const img = imgs[i];
-        img.alt = alts[i]
-
+    function getAltArray(number = 1) {
+      const url = "https://random-word-api.herokuapp.com/word?number=";
+      return fetch(url + number).then((response) => response.json());
     }
 
+    function changeImgAlt(image, altText) {
+      image.alt = altText;
+    }
 
-};
+    function createModalInputGroup(image) {
+      disableBodyScrolling();
+      const changeAtlGroup = document.createElement("div");
+      changeAtlGroup.id = "change-alt-group";
+      changeAtlGroup.style.cssText =
+        "position:fixed; z-index: 9999; top: 0; right: 0; bottom: 0; left: 0; background:rgba(0,0,0,.1)";
+      changeAtlGroup.addEventListener("click", removeModalInputGroup);
+
+      const changeAltInput = document.createElement("input");
+
+      changeAltInput.id = "change-alt-input";
+      changeAltInput.type = "text";
+      changeAltInput.value = image.alt;
+      changeAltInput.addEventListener("input", function () {
+        changeImgAlt(image, changeAltInput.value);
+      });
+
+      //add style to input
+
+      changeAtlGroup.appendChild(changeAltInput);
+
+      changeAtlGroup.addEventListener("keypress", function (e) {
+        if (e.key === "Enter") {
+          removeModalInputGroup();
+        }
+      });
+      document.body.appendChild(changeAtlGroup);
+      changeAltInput.focus();
+    }
+
+    function removeModalInputGroup() {
+      const changeAltInput = document.getElementById("change-alt-input");
+
+      changeAltInput.removeEventListener("input", function () {
+        changeImgAlt(image, changeAltInput.value);
+      });
+      changeAltInput.remove();
+
+      const changeAtlGroup = document.getElementById("change-alt-group");
+      changeAtlGroup.removeEventListener("click", removeModalInputGroup);
+      
+      changeAtlGroup.removeEventListener("keypress", function () {
+        if (e.key === "Enter") {
+          removeModalInputGroup();
+        }
+      });
+
+      changeAtlGroup.remove();
+      
+      enableBodyScrolling();
+    }
+
+    function addModalInputGroup(image) {
+      image.addEventListener("click", function () {
+        createModalInputGroup(image);
+      });
+    }
+
+    const images = document.querySelectorAll("img");
+    const imagesLength = images.length;
+    const altTextArray = await getAltArray(imagesLength);
+
+    for (let i = 0; i < imagesLength; i += 1) {
+      const currentImage = images[i];
+      changeImgAlt(currentImage, altTextArray[i]);
+      addModalInputGroup(currentImage);
+    }
+  };
 })();
